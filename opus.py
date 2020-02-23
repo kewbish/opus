d94fc48ed04f8adaa96ff897e562673e7a0b723e
Kewbish <kewbish@gmail.com>
2020-02-22 20:04:44 -0800

    Migrating to KV Design Lang

--- opus.py
+++ opus.py
@@ -2,26 +2,19 @@ import json
 import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.core.window import Window
 
 kivy.require('1.11.1')
 
 
-class main_page(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 1
-        self.add_widget(Label(text='OPUS - what have you read today?'))
-        self.add_widget(Button(text='the same thing'))
-        self.add_widget(Button(text='something new'))
+class MainPage(GridLayout):
+    pass
 
 
 class Opus(App):
 
     def build(self):
-        return main_page()
+        return MainPage()
 
 
 if __name__ == '__main__':
