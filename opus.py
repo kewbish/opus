385c70c596dddaa27a2da285015f446893281717
Kewbish <kewbish@gmail.com>
2020-03-01 19:59:34 -0800

    Beginning of same function

--- opus.py
+++ opus.py
@@ -1,4 +1,5 @@
-import json
+from datetime import datetime
+from json import load, dump
 import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
@@ -15,6 +16,15 @@ class Opus(App):
 
     def build(self):
         return MainPage()
+
+    def same_thing(self):
+        with open("data.json", "r") as x:
+            d = load(x)
+        now = datetime.now()
+        date = datetime.strftime(now, "%B %d %Y")
+        d["Bonus Poetry"]["date_ended"] = date
+        with open("data.json", "w") as x:
+            dump(d, x)
 
 
 if __name__ == '__main__':
