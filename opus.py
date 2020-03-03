4e12d46ed49bce7fa4bff90ad28a8667aa212a77
Kewbish <kewbish@gmail.com>
2020-03-02 19:57:28 -0800

    Began layout of second screen

--- opus.py
+++ opus.py
@@ -4,20 +4,30 @@ import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
+from kivy.properties import BooleanProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from requests import get
 
 kivy.require('1.11.1')
 
 
-class MainPage(GridLayout):
+class MainPage(Screen):
+    acc_shown = BooleanProperty(False)
+
+
+class SomethingElsePage(Screen):
     pass
 
 
 class Opus(App):
 
     def build(self):
-        return MainPage()
+        sm = ScreenManager()
+        sm.add_widget(MainPage(name='main_page'))
+        sm.add_widget(SomethingElsePage(name='something_else_page'))
+        return sm
 
-    def same_thing(self):
+    def same_thing_button(self):
         rates = []
         with open("data.csv", "r+") as x:
             r = reader(x)
@@ -29,6 +39,15 @@ class Opus(App):
             rates = [d[0], d[1], d[2], t]
             x.seek(0, 0)
             x.write(",".join(rates))
+
+    def get_author(book):
+        d = get(f"https://www.googleapis.com/books/v1/volumes?q={book}")
+        auth = d['items'][0]['authors'][0]
+        return auth
+
+    def something_else_button(self):
+        with open("data.csv", "r+") as x:
+            x.seek(0, 0)
 
 
 if __name__ == '__main__':
