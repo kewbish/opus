1ddf069ce6640dc7f25532f18f63a3feebbf3e09
Kewbish <kewbish@gmail.com>
2020-03-03 19:55:27 -0800

    MVP completed

--- opus.py
+++ opus.py
@@ -2,9 +2,8 @@ from csv import reader
 from datetime import datetime
 import kivy
 from kivy.app import App
+from kivy.properties import ObjectProperty
 from kivy.uix.gridlayout import GridLayout
-from kivy.core.window import Window
-from kivy.properties import BooleanProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from requests import get
 
@@ -12,11 +11,39 @@ kivy.require('1.11.1')
 
 
 class MainPage(Screen):
-    acc_shown = BooleanProperty(False)
+
+    def same_thing_button(self):
+        rates = []
+        with open("data.csv", "r+") as x:
+            r = reader(x)
+            d = [str(x).strip() for x in next(r)]
+            alls = x.readlines()
+            x.writelines(alls[1:])
+            t = datetime.strftime(datetime.now(), "%B %d %Y")
+            rates = [d[0], d[1], d[2], t]
+            x.seek(0, 0)
+            x.write(",".join(rates))
 
 
 class SomethingElsePage(Screen):
-    pass
+
+    def parse_book(self, value):
+        b = value[1].replace(" ", "_")
+        d = get(f"https://www.googleapis.com/books/v1/volumes?q={b}").\
+            json()
+        a = d['items'][0]['volumeInfo']['authors'][0]
+        auth = ObjectProperty()
+        self.ids.author.text = a
+
+    def enter_data(self):
+        t = datetime.strftime(datetime.now(), "%B %d %Y")
+        rates = [self.ids.book.text, self.ids.author.text, t, t]
+        with open("data.csv", "r+") as x:
+            alls = x.readlines()
+            x.seek(0, 0)
+            x.write(",".join(rates) + "\n")
+            x.writelines(alls)
+        self.parent.current = "main_page"
 
 
 class Opus(App):
@@ -26,31 +53,6 @@ class Opus(App):
         sm.add_widget(MainPage(name='main_page'))
         sm.add_widget(SomethingElsePage(name='something_else_page'))
         return sm
-
-    def same_thing_button(self):
-        rates = []
-        with open("data.csv", "r+") as x:
-            r = reader(x)
-            d = next(r)
-            alls = x.readlines()
-            x.writelines(alls[1:])
-            date = datetime.now()
-            t = datetime.strftime(date, " %B %d %Y")
-            rates = [d[0], d[1], d[2], t]
-            x.seek(0, 0)
-            x.write(",".join(rates))
-
-    def get_author(book):
-        d = get(f"https://www.googleapis.com/books/v1/volumes?q={book}")
-        auth = d['items'][0]['authors'][0]
-        return auth
-
-    def something_else_button(self):
-        with open("data.csv", "r+") as x:
-            x.seek(0, 0)
-
-    def parse_text(self, value):
-        return value[1]
 
 
 if __name__ == '__main__':
