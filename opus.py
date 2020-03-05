788cfdb210b2214c57f6c08e639728dd0cf39eea
Kewbish <kewbish@gmail.com>
2020-03-05 11:39:32 -0800

    Fixed accordion bug

--- opus.py
+++ opus.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.properties import ObjectProperty
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from os import chdir, path
 from requests import get
 
 kivy.require('1.11.1')
@@ -23,6 +24,11 @@ class MainPage(Screen):
             rates = [d[0], d[1], d[2], t]
             x.seek(0, 0)
             x.write(",".join(rates))
+
+    def something_else_button(self):
+        self.manager.get_screen("something_else_page").\
+            ids.book_item.collapse = False
+        self.manager.current = "something_else_page"
 
 
 class SomethingElsePage(Screen):
@@ -56,4 +62,6 @@ class Opus(App):
 
 
 if __name__ == '__main__':
+    if not path.isfile("data.csv"):
+        open("data.csv", "w+")
     Opus().run()
